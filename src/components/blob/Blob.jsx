308c930210abcd91f8d4aeea2364544cfcea0bcf
tushar-droid <tusharsandhu3022@gmail.com>
2025-05-03 16:22:13 -0700

    fixed transition for elements coming outside dropped zone.

--- src/components/blob/Blob.jsx
+++ src/components/blob/Blob.jsx
@@ -6,9 +6,13 @@ const Blob = (props) => {
   const { isOver, setNodeRef } = useDroppable({
     id: "drop-item",
   });
+
   return (
-    <div className="blob-container" id="drop-item" ref={setNodeRef}>
-      <div className="blob">{props.children}</div>
+    <div className="blob-container">
+      <div className="dropped-items-container" id="drop-item" ref={setNodeRef}>
+        {props.children}
+      </div>
+      <div className="blob"></div>
     </div>
   );
 };
